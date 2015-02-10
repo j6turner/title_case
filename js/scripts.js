3f4b0c490d548ce173731137e0df2e7894bc4bb9
Carli Martinez <martinezcarli@gmail.com>
2015-02-10 12:04:17 -0800

    Add second spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-var titleCase = function(word) {
-  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+var titleCase = function(sentence) {
+  var result = []
+  var words = sentence.split(" ");
+  words.forEach(function(word) {
+    result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+  });
+  return result.join(" ");
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe("titleCase", function() {
+
   it("capitalizes the first letter of a word and downcases the rest", function() {
     expect(titleCase("caT")).to.equal("Cat");
   });
+
+  it("capitalizes the first letter of each word and downcases the rest", function () {
+    expect(titleCase("caT i loVe")).to.equal("Cat I Love");
+  });
+
 });
