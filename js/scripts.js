b5ce0d92c697538354a9c652e77100c6c7b216de
Carli Martinez <martinezcarli@gmail.com>
2015-02-10 15:42:44 -0800

    Outline exception functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 var titleCase = function(sentence) {
-  var exception = ["a", "an", "and", "at", "behind", "beneath", "but", "by", "for", "from", "in", "into",
+  var exceptions = ["a", "an", "and", "at", "behind", "beneath", "but", "by", "for", "from", "in", "into",
   "nor", "of", "off", "on", "onto", "or", "out", "over", "up", "the", "to", "with"]
   var result = []
   var words = sentence.split(" ");
-  words.forEach(function(word) {
-    if exception !== indexOf(words[0]) {
-      result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+  var firstword = words.shift();
+
+  exceptions.forEach(function(exception) {
+    if firstword = exception
+      result.push(firstword.charAt(0).toUpperCase() + firstword.slice(1).toLowerCase());
+  })
+
     } else {
-      result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) && exception.toLowerCase()
+      words.forEach(function(word) {
+      result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     }
   });
   return result.join(" ");
